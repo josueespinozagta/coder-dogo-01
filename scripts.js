766e36e7006dabdb3280e700a87046e513a76a0c
josueespinozagta <jespinozaalcasihuincha@gmail.com>
2024-10-05 11:16:50 -0500

    coder dogo 02

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.querySelector('#boton-cambiar').addEventListener('click', function() {
+    document.querySelector('h1').textContent = "La rivalidad de los Goat";
+    document.querySelector('img').classList.add('imagen-destacada');
+});
